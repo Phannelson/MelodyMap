f6663b16cf7ac281afb76b57cb39739681ade949
Phannelson <70175005+Phannelson@users.noreply.github.com>
2024-05-11 15:36:17 -0700

    Set up simple login format

--- src/routes/Login/+page.svelte
+++ src/routes/Login/+page.svelte
@@ -1,9 +1,14 @@
 <div>
-<h1>Sign up</h1>
-<form>
-    <label>
-        <input type="text" placeholder="Email"/>
-    </label>
-</form>
-
+    <h1>Sign up</h1>
+    <form>
+        <label>
+            Email
+            <input type="text" placeholder="Email"/>
+        </label>
+        <label>
+            Password
+            <input type="password" placeholder="Password"/>
+        </label>
+        <button type="submit">Login</button>
+    </form>
 </div>
