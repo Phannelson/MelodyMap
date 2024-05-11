4f923d1e0ebb98238d6c7bbd53a963a9543096dd
Phannelson <70175005+Phannelson@users.noreply.github.com>
2024-05-11 08:48:38 -0700

    imports for firebase

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,21 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv'; // Import dotenv plugin
+import path from 'path'; // Import path for resolving directories
 
 dotenv.config(); // Load environment variables from .env file
 
 export default defineConfig({
-    plugins: [enhancedImages(), sveltekit()],
+    plugins: [
+        enhancedImages(),
+        sveltekit() // SvelteKit's Vite plugin
+    ],
+    resolve: {
+        alias: {
+            $components: path.resolve('./src/components'), // Alias for components
+            $lib: path.resolve('./src/lib'), // Alias for lib
+            $stores: path.resolve('./src/stores') // Alias for stores
+        }
+    }
     // other configurations...
 });
-
